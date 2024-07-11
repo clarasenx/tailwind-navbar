3809c213e6cf67821511565a20e5c8e371044f6f
Clara Helena <clarahelenasena@gmail.com>
2024-07-11 13:13:28 -0400

    vite update

--- frontend/main.js
+++ frontend/main.js
@@ -1,24 +1,44 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.js'
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" className="text-zinc-200" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+document.querySelector("#app").innerHTML = `
+  <main class="font-[Poppins] bg-gradient-to-t from-[#fbc2eb] to-[#a6c1ee] h-screen">
+    <header class="bg-white">
+        <nav class="flex justify-between items-center w-[92%] sm:h-24 h-32 mx-auto py-2">
+            <div>
+                <!-- <img class="w-16 fill-[#fbc2eb]"src="/assets/flower-outline.svg" alt=""> -->
+                <ion-icon class="text-6xl text-[#fbc2eb]" name="flower-outline"></ion-icon>
+            </div>
+            <div class="navLinks sm:static absolute bg-white sm:min-h-fit min-h-[40vh] left-0 top-[12%] w-full flex items-center place-content-center px-5">
+                <ul class="flex sm:flex-row flex-col items-center sm:gap-[4vw] gap-8">
+                    <li>
+                        <a class="hover:text-gray-500" href="#">Home</a>
+                    </li>
+                    <li>
+                        <a class="hover:text-gray-500" href="#">Resources</a>
+                    </li>
+                    <li>
+                        <a class="hover:text-gray-500" href="#">Products</a>
+                    </li>
+                    <li>
+                        <a class="hover:text-gray-500" href="#">About Us</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="flex items-center">
+                <button class="bg-[#a6c1ee] mr-3 w-24 text-sm text-white px-5 py-1 rounded-full hover:bg-[#87acec]">Sign in</button>
+                <ion-icon onclick="hideMenu(this)" class="text-3x1 cursor-pointer sm:hidden" name="menu"></ion-icon>
+            </div>
+        </nav>
+    </header>
+  </main>
+  <script>
+  function hideMenu(e) {
+  const navLinks = document.querySelector(".navLinks");
+  e.name = e.name === "menu" ? "close" : "menu";
+  navLinks.classList.toggle("invisible");
+}
+</script>
+`;
 
 setupCounter(document.querySelector('#counter'))
